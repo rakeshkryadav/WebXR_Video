3b773f679e88bf25c0593bf8e0102edf95d07cdb
Rakesh Kr Yadav <rakesh.kr.yadav.008@gmail.com>
2026-07-27 13:57:43 +0530

    Font size

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import * as THREE from "three";
 import { MindARThree } from "https://cdn.jsdelivr.net/npm/mind-ar@1.2.5/dist/mindar-image-three.prod.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
-console.log("test 16");
+console.log("test 17");
 
 const mindarThree = new MindARThree({
     container: document.body,
@@ -121,7 +121,7 @@ for (let i = 0; i < 3; i++) {
 
         // Background
         context.fillStyle = "rgba(0,0,0,0.85)";
-        context.fillRect(0, 0, canvas.width, canvas.height);
+        context.roundRect(0, 0, canvas.width, canvas.height, 30);
 
         // Text
         context.fillStyle = "white";
@@ -161,7 +161,7 @@ for (let i = 0; i < 3; i++) {
     );
 
     // Height of the sprite in world units
-    const height = 0.15;
+    const height = 1;
 
     // Width is automatically adjusted
     textMesh.scale.set(height * textData.aspect, height, 1);
