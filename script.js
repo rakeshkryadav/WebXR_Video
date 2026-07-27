97719f5198f144fee2dd6ff584a90e42dc891ca6
Rakesh Kr Yadav <rakesh.kr.yadav.008@gmail.com>
2026-07-27 15:53:31 +0530

    Update filterBeta to 0

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const mindarThree = new MindARThree({
     container: document.body,
     imageTargetSrc: "cards.mind",
     filterMinCF: 0.001,         // default: 0.001   (decrease the value to make it less jittery)
-    filterBeta: 5,             // default: 1000    (increase the value to reduce the delay)
+    filterBeta: 0,             // default: 1000    (increase the value to reduce the delay)
     warmupTolerance: 5,          // default: 5
     missTolerance: 0,           // default: 5
 });
