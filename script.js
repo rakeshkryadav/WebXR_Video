64ff5f7b72217f643f551217154498c294f9d037
Rakesh Kr Yadav <rakesh.kr.yadav.008@gmail.com>
2026-07-30 09:58:33 +0530

    Update warmupTolerance and missTolerance

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ const mindarThree = new MindARThree({
     imageTargetSrc: "cards.mind",
     filterMinCF: 0.001,         // default: 0.001   (decrease the value to make it less jittery)
     filterBeta: 0,             // default: 1000    (increase the value to reduce the delay)
-    warmupTolerance: 5,          // default: 5
-    missTolerance: 0,           // default: 5
+    warmupTolerance: 50,          // default: 5
+    missTolerance: 5,           // default: 5
 });
 
 const { renderer, scene, camera } = mindarThree;
